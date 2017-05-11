120716b8de0099190844778cf73127d8718a971a
Gan <liganok86@qq.com>
2017-05-11 15:44:36 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import Mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import {default as Logger} from 'morgan';
+
+import Config from './config/config';
+
+let app = express();
+
+// Normal express config defaults
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+//app.use(session({ secret: 'meetingku', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+Mongoose.connect(Config.database);
+Mongoose.connection.on('error', function () {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+});
+
+app.set('port', process.env.PORT || 3001);
+app.use(Logger('dev'));
+
+
+app.use(require('./routes'));
+
+app.get('/api/test', function (req, res, next) {
+    res.send('helloword');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
